e61e9e821289425846f4484ff300f8aebd0b03cb
diegob94 <dbim.bena@gmail.com>
2021-04-17 16:15:16 -0400

    override entrypoint

--- chisel.sh
+++ chisel.sh
@@ -4,4 +4,6 @@ mkdir -p ~/.ivy2
 mkdir -p ~/.sbt
 mkdir -p ~/.cache
 
-podman run --rm -it -v $PWD:/work:Z -v ~/.ivy2:/root/.ivy2:Z -v ~/.sbt:/root/.sbt:Z -v ~/.cache:/root/.cache:Z diegob94/open_eda:chisel "$@"
+#shell="--entrypoint bash"
+
+podman run --rm -it $shell -v $PWD:/work:Z -v ~/.ivy2:/root/.ivy2:Z -v ~/.sbt:/root/.sbt:Z -v ~/.cache:/root/.cache:Z diegob94/open_eda:chisel "$@"
