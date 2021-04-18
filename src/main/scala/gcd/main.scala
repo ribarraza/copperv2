847320fa4f9da599a5e1e0683f893c6da9be24cc
diegob94 <dbim.bena@gmail.com>
2021-04-17 21:17:59 -0400

    ChiselStage use emitVerilog instead of execute

--- src/main/scala/gcd/main.scala
+++ src/main/scala/gcd/main.scala
@@ -3,6 +3,6 @@ package gcd
 import chisel3.stage.{ChiselStage, ChiselGeneratorAnnotation}
 
 object GCDDriver extends App {
-  (new ChiselStage).execute(args,
-    Seq(ChiselGeneratorAnnotation(() => new GCD)))
+  (new ChiselStage).emitVerilog(new DecoupledGcd(4), args)
+  (new ChiselStage).emitVerilog(new GCD, args)
 }
