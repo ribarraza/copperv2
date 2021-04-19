a56237f0a05d8e372808a3a1946702f4f0855567
diegob94 <dbim.bena@gmail.com>
2021-04-18 22:06:25 -0400

    Add -it for using Scala REPL

--- scripts/chisel.sh
+++ scripts/chisel.sh
@@ -4,6 +4,6 @@ mkdir -p ~/.ivy2
 mkdir -p ~/.sbt
 mkdir -p ~/.cache
 
-#shell="-it --entrypoint bash"
+#shell="--entrypoint bash"
 
-podman run --rm $shell -v $PWD:/work:Z -v ~/.ivy2:/root/.ivy2:Z -v ~/.sbt:/root/.sbt:Z -v ~/.cache:/root/.cache:Z diegob94/open_eda:chisel "$@"
+podman run --rm $shell -it -v $PWD:/work:Z -v ~/.ivy2:/root/.ivy2:Z -v ~/.sbt:/root/.sbt:Z -v ~/.cache:/root/.cache:Z diegob94/open_eda:chisel "$@"
