51cd8a692cba4d70086ab920c6355644da9e8535
diegob94 <dbim.bena@gmail.com>
2021-04-20 00:13:42 -0400

    Remove unused ports

--- src/main/scala/copperv2/copperv2.scala
+++ src/main/scala/copperv2/copperv2.scala
@@ -12,52 +12,46 @@ class Cuv2Config {
   var bus = new BusConfig()
 }
 
-class Cuv2ReadChannel(data_width: Int, addr_width: Int) extends Bundle {
+class Cuv2ReadChannel(config: Cuv2Config) extends Bundle {
   // Output
-  val addr = Decoupled(UInt(addr_width.W))
+  val addr = Decoupled(UInt(config.bus.addr_width.W))
   // Input
-  val data = Flipped(Decoupled(UInt(data_width.W)))
+  val data = Flipped(Decoupled(UInt(config.bus.data_width.W)))
 }
 
-class Cuv2WriteChannel(data_width: Int, addr_width: Int, resp_width: Int) extends Bundle {
-  val strobe_width = data_width / 4
+class Cuv2WriteOutput(config: Cuv2Config) extends Bundle {
+  val strobe_width = config.bus.data_width / 4
+  val data = UInt(config.bus.data_width.W)
+  val addr = UInt(config.bus.addr_width.W)
+  val strobe = UInt(strobe_width.W)
+}
+
+class Cuv2WriteChannel(config: Cuv2Config) extends Bundle {
   // Output
-  val data = Decoupled(UInt(data_width.W))
-  val addr = Decoupled(UInt(addr_width.W))
-  val strobe = Decoupled(UInt(strobe_width.W))
+  val req = Decoupled(new Cuv2WriteOutput(config))
   // Input
-  val resp = Flipped(Decoupled(UInt(resp_width.W)))
+  val resp = Flipped(Decoupled(UInt(config.bus.resp_width.W)))
 }
 
 class Copperv2Bus(config: Cuv2Config) extends Bundle {
-  val ir = new Cuv2ReadChannel(config.bus.data_width,config.bus.addr_width)
-  val iw = new Cuv2WriteChannel(config.bus.data_width,config.bus.addr_width,config.bus.resp_width)
-  val dr = new Cuv2ReadChannel(config.bus.data_width,config.bus.addr_width)
-  val dw = new Cuv2WriteChannel(config.bus.data_width,config.bus.addr_width,config.bus.resp_width)
+  val ir = new Cuv2ReadChannel(config)
+  val dr = new Cuv2ReadChannel(config)
+  val dw = new Cuv2WriteChannel(config)
 }
 
 class Copperv2 extends MultiIOModule {
   val config = new Cuv2Config
-  val io = IO(new Copperv2Bus(config))
-  io.dw.strobe.bits := 0.U
-  io.dw.data.bits := 0.U
-  io.dw.data.valid := 0.U
-  io.ir.addr.valid := 0.U
-  io.iw.data.valid := 0.U
-  io.dw.addr.valid := 0.U
-  io.dw.strobe.valid := 0.U
-  io.dr.addr.valid := 0.U
-  io.dw.addr.bits := 0.U
-  io.iw.addr.valid := 0.U
-  io.iw.strobe.bits := 0.U
-  io.iw.data.bits := 0.U
-  io.ir.data.ready := 0.U
-  io.dr.addr.bits := 0.U
-  io.iw.addr.bits := 0.U
-  io.dr.data.ready := 0.U
-  io.iw.resp.ready := 0.U
-  io.dw.resp.ready := 0.U
-  io.ir.addr.bits := 0.U
-  io.iw.strobe.valid := 0.U
+  val bus = IO(new Copperv2Bus(config))
+  bus.ir.addr.valid := 0.U
+  bus.ir.addr.bits := 0.U
+  bus.ir.data.ready := 0.U
+  bus.dr.addr.valid := 0.U
+  bus.dr.addr.bits := 0.U
+  bus.dr.data.ready := 0.U
+  bus.dw.req.valid := 0.U
+  bus.dw.req.bits.data := 0.U
+  bus.dw.req.bits.addr := 0.U
+  bus.dw.req.bits.strobe := 0.U
+  bus.dw.resp.ready := 0.U
 }
 
