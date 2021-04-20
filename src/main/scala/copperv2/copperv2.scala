89ead606d66d11f99aa0e1c71903721c8022d915
diegob94 <dbim.bena@gmail.com>
2021-04-19 22:49:26 -0400

    Interface progress

--- src/main/scala/copperv2/copperv2.scala
+++ src/main/scala/copperv2/copperv2.scala
@@ -4,29 +4,45 @@ import chisel3._
 import chisel3.util.Decoupled
 
 class Cuv2Config {
-  object bus {
-    val data_width = 32
-    val addr_width = 32
-    val resp_width = 1
-    val strobe_width = data_width/8
-  }
+  class BusConfig(
+    data_width: Int = 32, 
+    addr_width: Int = 32, 
+    resp_width: Int = 1
+  )
+  val bus = new BusConfig()
 }
 
-class Cuv2InputBundle(config: Cuv2Config) extends Bundle {
-  val dr_data = UInt(config.bus.data_width.W)
-  val dw_resp = UInt(config.bus.resp_width.W)
-}
+//class Cuv2ReadChannel(data_width: Int, addr_width: Int) extends Bundle {
+//  // Output
+//  val addr = Decoupled(UInt(addr_width.W))
+//  // Input
+//  val data = Flipped(Decoupled(UInt(data_width.W)))
+//}
+//
+//class Cuv2WriteChannel(data_width: Int, addr_width: Int, resp_width: Int) extends Bundle {
+//  val strobe_width = data_width / 4
+//  printf(p"strobe_width = $strobe_width")
+//  // Output
+//  val data = Decoupled(UInt(data_width.W))
+//  val addr = Decoupled(UInt(addr_width.W))
+//  val strobe = Decoupled(UInt(strobe_width.W))
+//  // Input
+//  val resp = Flipped(Decoupled(UInt(resp_width.W)))
+//}
 
-class Cuv2OutputBundle(config: Cuv2Config) extends Bundle {
-  val dr_addr = UInt(config.bus.addr_width.W)
-  val dw_addr = UInt(config.bus.addr_width.W)
-  val dw_data = UInt(config.bus.addr_width.W)
-  val dw_strobe = UInt(config.bus.strobe_width.W)
-}
+//class Cuv2BusChannel(config: Cuv2Config#BusConfig) extends Bundle {
+//  val r = new Cuv2ReadChannel(config.data_width,config.addr_width)
+//  val w = new Cuv2WriteChannel(config.data_width,config.addr_width,config.resp_width)
+//}
 
+class Copperv2Bus(config: Cuv2Config) extends Bundle {
+  //val i = new Cuv2BusChannel(config.bus)
+  //val d = new Cuv2BusChannel(config.bus)
+  printf(p"config.bus.data_width = ${config.bus.data_width}")
+}
 
 class Copperv2 extends MultiIOModule {
   val config = new Cuv2Config
-  val input = IO(Flipped(Decoupled(new Cuv2InputBundle(config))))
-  val output = IO(Decoupled(new Cuv2OutputBundle(config)))
+  val io = IO(new Copperv2Bus(config))
 }
+
