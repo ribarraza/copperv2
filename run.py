a8504aaa2fcb15d7ec671dfa49d4ed8d226b7708
diegob94 <dbim.bena@gmail.com>
2021-04-19 22:45:52 -0400

    Chisel interactive

--- run.py
+++ run.py
@@ -20,6 +20,18 @@ class Unbuffered:
        return getattr(self.stream, attr)
 sys.stdout = Unbuffered(sys.stdout)
 
+def run(c,run_opts,step):
+    print(f"run.py-info: {step}> {c}")
+    try:
+        subprocess.run(c,**run_opts)
+    except KeyboardInterrupt:
+        time.sleep(1)
+        print('\nrun.py-error: Keyboard interrupt')
+        sys.exit(1)
+    except subprocess.CalledProcessError:
+        print('run.py-error: Called process error')
+        sys.exit(1)
+
 root=Path('.')
 work_dir=root/'work'
 work_dir.mkdir(exist_ok=True)
@@ -28,7 +40,7 @@ chisel_work_dir=work_dir/'chisel'
 sim_work_dir=work_dir/'sim'
 
 steps = dict(
-    chisel = f'./scripts/chisel.sh "runMain Copperv2Driver --target-dir {chisel_work_dir}"',
+    chisel = f'./scripts/chisel.sh sbt "runMain Copperv2Driver --target-dir {chisel_work_dir}"',
     sim_build = [
         f"./scripts/verilator.sh cmake -f ./sim/CMakeLists.txt -B {sim_work_dir}",
         f"./scripts/verilator.sh cmake --build {sim_work_dir} --parallel $(nproc)",
@@ -42,12 +54,19 @@ parser.add_argument('-from', type=str, choices=step_list, default=step_list[0], 
                     help='Run from this step')
 parser.add_argument('-to', type=str, choices=step_list, default=step_list[-1],
                     help='Run to this step')
+parser.add_argument('-chisel', action='store_true',
+                    help='Launch chisel interactive shell (sbt)')
 
 args = parser.parse_args()
 
+default_run_opts = dict(shell=True,check=True,encoding='utf-8')
+if args.chisel:
+    run('./scripts/chisel.sh sbt',default_run_opts,'chisel_shell')
+    sys.exit(0)
+
 for step in step_list[step_list.index(args._from):step_list.index(args.to)+1]:
     commands = steps[step]
-    run_opts = dict(shell=True,check=True,encoding='utf-8')
+    run_opts = default_run_opts
     if isinstance(commands,dict):
         cmd = commands.pop('args')
         run_opts.update(commands)
@@ -56,15 +75,6 @@ for step in step_list[step_list.index(args._from):step_list.index(args.to)+1]:
     if isinstance(cmd,str):
         cmd = [cmd]
     for c in cmd:
-        print(f"run.py-info: {step}> {c}")
-        try:
-            subprocess.run(c,**run_opts)
-        except KeyboardInterrupt:
-            time.sleep(1)
-            print('\nrun.py-error: Keyboard interrupt')
-            sys.exit(1)
-        except subprocess.CalledProcessError:
-            print('run.py-error: Called process error')
-            sys.exit(1)
+        run(c,run_opts,step)
 
 
