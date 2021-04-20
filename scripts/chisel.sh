34516faf7efab1b650fe0734e1a2d536e843ff00
diegob94 <dbim.bena@gmail.com>
2021-04-19 23:12:53 -0400

    Remove entrypoint override

--- scripts/chisel.sh
+++ scripts/chisel.sh
@@ -4,6 +4,4 @@ mkdir -p ~/.ivy2
 mkdir -p ~/.sbt
 mkdir -p ~/.cache
 
-#shell="--entrypoint bash"
-
 podman run --rm $shell -it -v $PWD:/work:Z -v ~/.ivy2:/root/.ivy2:Z -v ~/.sbt:/root/.sbt:Z -v ~/.cache:/root/.cache:Z diegob94/open_eda:chisel "$@"
