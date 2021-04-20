d8e98516b66095eda5ac50bc0ec9e29405e33ef0
diegob94 <dbim.bena@gmail.com>
2021-04-19 23:51:55 -0400

    Interface working

--- src/main/scala/copperv2/copperv2.scala
+++ src/main/scala/copperv2/copperv2.scala
@@ -5,44 +5,59 @@ import chisel3.util.Decoupled
 
 class Cuv2Config {
   class BusConfig(
-    data_width: Int = 32, 
-    addr_width: Int = 32, 
-    resp_width: Int = 1
+    var data_width: Int = 32, 
+    var addr_width: Int = 32, 
+    var resp_width: Int = 1
   )
-  val bus = new BusConfig()
+  var bus = new BusConfig()
 }
 
-//class Cuv2ReadChannel(data_width: Int, addr_width: Int) extends Bundle {
-//  // Output
-//  val addr = Decoupled(UInt(addr_width.W))
-//  // Input
-//  val data = Flipped(Decoupled(UInt(data_width.W)))
-//}
-//
-//class Cuv2WriteChannel(data_width: Int, addr_width: Int, resp_width: Int) extends Bundle {
-//  val strobe_width = data_width / 4
-//  printf(p"strobe_width = $strobe_width")
-//  // Output
-//  val data = Decoupled(UInt(data_width.W))
-//  val addr = Decoupled(UInt(addr_width.W))
-//  val strobe = Decoupled(UInt(strobe_width.W))
-//  // Input
-//  val resp = Flipped(Decoupled(UInt(resp_width.W)))
-//}
+class Cuv2ReadChannel(data_width: Int, addr_width: Int) extends Bundle {
+  // Output
+  val addr = Decoupled(UInt(addr_width.W))
+  // Input
+  val data = Flipped(Decoupled(UInt(data_width.W)))
+}
 
-//class Cuv2BusChannel(config: Cuv2Config#BusConfig) extends Bundle {
-//  val r = new Cuv2ReadChannel(config.data_width,config.addr_width)
-//  val w = new Cuv2WriteChannel(config.data_width,config.addr_width,config.resp_width)
-//}
+class Cuv2WriteChannel(data_width: Int, addr_width: Int, resp_width: Int) extends Bundle {
+  val strobe_width = data_width / 4
+  // Output
+  val data = Decoupled(UInt(data_width.W))
+  val addr = Decoupled(UInt(addr_width.W))
+  val strobe = Decoupled(UInt(strobe_width.W))
+  // Input
+  val resp = Flipped(Decoupled(UInt(resp_width.W)))
+}
 
 class Copperv2Bus(config: Cuv2Config) extends Bundle {
-  //val i = new Cuv2BusChannel(config.bus)
-  //val d = new Cuv2BusChannel(config.bus)
-  printf(p"config.bus.data_width = ${config.bus.data_width}")
+  val ir = new Cuv2ReadChannel(config.bus.data_width,config.bus.addr_width)
+  val iw = new Cuv2WriteChannel(config.bus.data_width,config.bus.addr_width,config.bus.resp_width)
+  val dr = new Cuv2ReadChannel(config.bus.data_width,config.bus.addr_width)
+  val dw = new Cuv2WriteChannel(config.bus.data_width,config.bus.addr_width,config.bus.resp_width)
 }
 
 class Copperv2 extends MultiIOModule {
   val config = new Cuv2Config
   val io = IO(new Copperv2Bus(config))
+  io.dw.strobe.bits := 0.U
+  io.dw.data.bits := 0.U
+  io.dw.data.valid := 0.U
+  io.ir.addr.valid := 0.U
+  io.iw.data.valid := 0.U
+  io.dw.addr.valid := 0.U
+  io.dw.strobe.valid := 0.U
+  io.dr.addr.valid := 0.U
+  io.dw.addr.bits := 0.U
+  io.iw.addr.valid := 0.U
+  io.iw.strobe.bits := 0.U
+  io.iw.data.bits := 0.U
+  io.ir.data.ready := 0.U
+  io.dr.addr.bits := 0.U
+  io.iw.addr.bits := 0.U
+  io.dr.data.ready := 0.U
+  io.iw.resp.ready := 0.U
+  io.dw.resp.ready := 0.U
+  io.ir.addr.bits := 0.U
+  io.iw.strobe.valid := 0.U
 }
 
