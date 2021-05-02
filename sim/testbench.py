695f212e0392bc6f01c37d37319dae6081ec0819
diegob94 <dbim.bena@gmail.com>
2021-05-02 12:32:34 -0400

    Move binds to TBConfig object

--- sim/testbench.py
+++ sim/testbench.py
@@ -7,63 +7,72 @@ from cocotb.triggers import RisingEdge, ClockCycles
 from bus import ReadBusMonitor, ReadBusSourceDriver, BusReadTransaction, BusWriteTransaction
 from regfile import RegFileReadMonitor, RegFileWriteMonitor, RegFileReadTransaction, RegFileWriteTransaction
 
+class TBConfig:
+    def __init__(self, dut):
+        self.dut = dut
+        self.clock = dut.clk
+        self.reset = dut.rst
+        self.copperv1_bind()
+    def copperv1_bind(self):
+        self.ir_bind = dict(
+            clock = self.clock,
+            addr_valid = self.dut.ir_addr_valid,
+            addr_ready = self.dut.ir_addr_ready,
+            addr = self.dut.ir_addr,
+            data_valid = self.dut.ir_data_valid,
+            data_ready = self.dut.ir_data_ready,
+            data = self.dut.ir_data,
+        )
+        self.dr_bind = dict(
+            clock = self.clock,
+            addr_valid = self.dut.dr_addr_valid,
+            addr_ready = self.dut.dr_addr_ready,
+            addr = self.dut.dr_addr,
+            data_valid = self.dut.dr_data_valid,
+            data_ready = self.dut.dr_data_ready,
+            data = self.dut.dr_data,
+        )
+        self.regfile_write_bind = dict(
+            clock = self.clock,
+            rd_en = self.dut.regfile.rd_en,
+            rd_addr = self.dut.regfile.rd,
+            rd_data = self.dut.regfile.rd_din,
+        )
+        self.regfile_read_bind = dict(
+            clock = self.clock,
+            rs1_en = self.dut.regfile.rs1_en,
+            rs1_addr = self.dut.regfile.rs1,
+            rs1_data = self.dut.regfile.rs1_dout,
+            rs2_en = self.dut.regfile.rs2_en,
+            rs2_addr = self.dut.regfile.rs2,
+            rs2_data = self.dut.regfile.rs2_dout,
+        )
+
 class Testbench():
     def __init__(self, dut, elf, params):
         self.log = SimLog(f"cocotb.Testbench")
-        self.clock = dut.clk
-        self.reset = dut.rst
+        config = TBConfig(dut)
+        self.clock = config.clock
+        self.reset = config.reset
         self.reset.setimmediatevalue(0)
-        ir_bind = dict(
-            clock = self.clock,
-            addr_valid = dut.ir_addr_valid,
-            addr_ready = dut.ir_addr_ready,
-            addr = dut.ir_addr,
-            data_valid = dut.ir_data_valid,
-            data_ready = dut.ir_data_ready,
-            data = dut.ir_data,
-        )
-        dr_bind = dict(
-            clock = self.clock,
-            addr_valid = dut.dr_addr_valid,
-            addr_ready = dut.dr_addr_ready,
-            addr = dut.dr_addr,
-            data_valid = dut.dr_data_valid,
-            data_ready = dut.dr_data_ready,
-            data = dut.dr_data,
-        )
-        regfile_write_bind = dict(
-            clock = self.clock,
-            rd_en = dut.regfile.rd_en,
-            rd_addr = dut.regfile.rd,
-            rd_data = dut.regfile.rd_din,
-        )
-        regfile_read_bind = dict(
-            clock = self.clock,
-            rs1_en = dut.regfile.rs1_en,
-            rs1_addr = dut.regfile.rs1,
-            rs1_data = dut.regfile.rs1_dout,
-            rs2_en = dut.regfile.rs2_en,
-            rs2_addr = dut.regfile.rs2,
-            rs2_data = dut.regfile.rs2_dout,
-        )
         ## Instruction read
         self.elf = elf
-        self.bus_ir_driver = ReadBusSourceDriver("bus_ir", ir_bind)
-        self.bus_ir_monitor = ReadBusMonitor("bus_ir", ir_bind, reset = self.reset)
-        self.bus_ir_req_monitor = ReadBusMonitor("bus_ir_req", ir_bind, request_only=True,
+        self.bus_ir_driver = ReadBusSourceDriver("bus_ir", config.ir_bind)
+        self.bus_ir_monitor = ReadBusMonitor("bus_ir", config.ir_bind, reset = self.reset)
+        self.bus_ir_req_monitor = ReadBusMonitor("bus_ir_req", config.ir_bind, request_only=True,
             callback=self.instruction_read_callback, reset = self.reset)
         ## Data read
         self.data_memory = {}
         for t in params.data_memory:
             t = BusReadTransaction.from_string(t)
             self.data_memory[t.addr] = t.data
-        self.bus_dr_driver = ReadBusSourceDriver("bus_dr", dr_bind)
-        self.bus_dr_monitor = ReadBusMonitor("bus_dr", dr_bind, reset = self.reset)
-        self.bus_dr_req_monitor = ReadBusMonitor("bus_dr_req", dr_bind, request_only=True,
+        self.bus_dr_driver = ReadBusSourceDriver("bus_dr", config.dr_bind)
+        self.bus_dr_monitor = ReadBusMonitor("bus_dr", config.dr_bind, reset = self.reset)
+        self.bus_dr_req_monitor = ReadBusMonitor("bus_dr_req", config.dr_bind, request_only=True,
             callback=self.data_read_callback, reset = self.reset)
         ## Regfile
-        self.regfile_write_monitor = RegFileWriteMonitor("regfile_write", regfile_write_bind)
-        self.regfile_read_monitor = RegFileReadMonitor("regfile_read", regfile_read_bind)
+        self.regfile_write_monitor = RegFileWriteMonitor("regfile_write", config.regfile_write_bind)
+        self.regfile_read_monitor = RegFileReadMonitor("regfile_read", config.regfile_read_bind)
         ## Self checking
         self.scoreboard = Scoreboard(dut)
         self.expected_regfile_read = [RegFileReadTransaction.from_string(t) for t in params.expected_regfile_read]
