5174127a0559d1cc7bf8e7afa952a377d3d055e5
diegob94 <dbim.bena@gmail.com>
2021-05-02 22:52:47 -0400

    Instantiate rtl_v1 blackboxes

--- src/main/scala/copperv2/copperv2.scala
+++ src/main/scala/copperv2/copperv2.scala
@@ -14,6 +14,67 @@ class Cuv2Config {
   var pc_init = 0
 }
 
+class idecoder extends BlackBox {
+  val io = IO(new Bundle {
+    val inst = Input(UInt(32.W))
+    val imm = Output(UInt(32.W))
+    val inst_type = Output(UInt(4.W))
+    val rd = Output(UInt(5.W))
+    val rs1 = Output(UInt(5.W))
+    val rs2 = Output(UInt(5.W))
+    val funct = Output(UInt(5.W))
+  })
+}
+
+class register_file extends BlackBox {
+  val io = IO(new Bundle {
+    val clk = Input(Bool())
+    val rst = Input(Bool())
+    val rd_en = Input(Bool())
+    val rs1_en = Input(Bool())
+    val rs2_en = Input(Bool())
+    val rd = Input(UInt(5.W))
+    val rs1 = Input(UInt(5.W))
+    val rs2 = Input(UInt(5.W))
+    val rd_din = Input(UInt(32.W))
+    val rs1_dout = Output(UInt(32.W))
+    val rs2_dout = Output(UInt(32.W))
+  })
+}
+
+class arith_logic_unit extends BlackBox {
+  val io = IO(new Bundle {
+    val alu_din1 = Input(UInt(32.W))
+    val alu_din2 = Input(UInt(32.W))
+    val alu_op = Input(UInt(4.W))
+    val alu_dout = Output(UInt(32.W))
+    val alu_comp = Output(UInt(3.W))
+  })
+}
+
+class control_unit extends BlackBox {
+  val io = IO(new Bundle {
+    val clk = Input(Bool())
+    val rst = Input(Bool())
+    val inst_type = Input(UInt(4.W))
+    val inst_valid = Input(Bool())
+    val alu_comp = Input(UInt(3.W))
+    val funct = Input(UInt(5.W))
+    val data_valid = Input(Bool())
+    val inst_fetch = Output(Bool())
+    val load_data = Output(Bool())
+    val store_data = Output(Bool())
+    val rd_en = Output(Bool())
+    val rs1_en = Output(Bool())
+    val rs2_en = Output(Bool())
+    val rd_din_sel = Output(UInt(2.W))
+    val pc_next_sel = Output(UInt(2.W))
+    val alu_din1_sel = Output(UInt(2.W))
+    val alu_din2_sel = Output(UInt(2.W))
+    val alu_op = Output(UInt(4.W))
+  })
+}
+
 class Cuv2ReadChannel(config: Cuv2Config) extends Bundle {
   // Output
   val addr = Decoupled(UInt(config.bus.addr_width.W))
@@ -64,5 +125,12 @@ class Copperv2 extends MultiIOModule with RequireSyncReset {
     bus.ir.addr.bits := pc
     bus.ir.addr.valid := true.B
   }
+//  val bb = Module(new BlackBoxRealAdd)
+//  bb.io.in1 := bus.ir.data.bits;
+//  bb.io.in2 := bus.dr.data.bits;
+//  bus.dr.addr.bits := bb.io.out;
+  val idec = Module(new idecoder)
+  val regfile = Module(new register_file)
+  val alu = Module(new arith_logic_unit)
+  val control = Module(new control_unit)
 }
-
